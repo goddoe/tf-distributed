d0390370ea1862a128a2180d1ee8e35be97b6d5f
Sung-ju Kim <goddoe@goddoe2@gmail.com>
2018-02-28 16:55:27 +0900

    make param json and validation code

--- tf_distributed.py
+++ tf_distributed.py
@@ -5,14 +5,14 @@ import tensorflow as tf
 
 from model import mlp
 from dataset import load_data
-from utils import calc_metric
+from utils import calc_metric, load_json
 
 slim = tf.contrib.slim
 
 FLAGS = None
-OUTPUT_DIM = 10
-BATCH_SIZE = 64
-N_EPOCH = 5
+
+VERBOSE_INTERVAL = 10  # by batch
+TRAIN_ACC_WINDOW_SIZE = 10
 
 
 def main(_):
@@ -27,6 +27,7 @@ def main(_):
                              job_name=FLAGS.job_name,
                              task_index=FLAGS.task_index)
     # Parameters 
+    param_dict = load_json(FLAGS.param_path)
 
     if FLAGS.job_name == "ps":
         server.join()
@@ -37,8 +38,12 @@ def main(_):
          Y_train,
          X_valid,
          Y_valid) = load_data()
+            
+        # Inference output dimension
+        output_dim = len(Y_train[0]) 
 
-        # output_dim = len(Y_train[0]) 
+        # check chief
+        is_chief = FLAGS.task_index == 0
 
         # Assigns ops to the local worker by default.
         with tf.device(tf.train.replica_device_setter(
@@ -50,12 +55,13 @@ def main(_):
             train_X_dataset = tf.data.Dataset.from_tensor_slices(X_train)
             train_Y_dataset = tf.data.Dataset.from_tensor_slices(Y_train)
             train_dataset = tf.data.Dataset.zip((train_X_dataset, train_Y_dataset))
-            train_dataset = train_dataset.shuffle(1000).batch(BATCH_SIZE).repeat(N_EPOCH)
+            train_dataset = train_dataset.shuffle(1000).batch(param_dict['batch_size']).repeat(param_dict['n_epoch'])
 
-            valid_X_dataset = tf.data.Dataset.from_tensor_slices(X_valid)
-            valid_Y_dataset = tf.data.Dataset.from_tensor_slices(Y_valid)
-            valid_dataset = tf.data.Dataset.zip((valid_X_dataset, valid_Y_dataset))
-            valid_dataset = valid_dataset.shuffle(1000).batch(BATCH_SIZE)
+            if is_chief:
+                valid_X_dataset = tf.data.Dataset.from_tensor_slices(X_valid)
+                valid_Y_dataset = tf.data.Dataset.from_tensor_slices(Y_valid)
+                valid_dataset = tf.data.Dataset.zip((valid_X_dataset, valid_Y_dataset))
+                valid_dataset = valid_dataset.shuffle(1000).batch(param_dict['batch_size'])
 
             # Feedable Iterator
             handle = tf.placeholder(tf.string, shape=[])
@@ -64,8 +70,11 @@ def main(_):
 
             # Iterators 
             train_iterator = train_dataset.make_one_shot_iterator()
-            # valid_iterator = valid_dataset.make_initializable_iterator()
             train_handle_tensor = train_iterator.string_handle()
+
+            if is_chief:
+                valid_iterator = valid_dataset.make_initializable_iterator()
+                valid_handle_tensor = valid_iterator.string_handle()
 
             X, Y = iterator.get_next()
             is_training = tf.placeholder_with_default(False,
@@ -75,11 +84,9 @@ def main(_):
             global_step = tf.contrib.framework.get_or_create_global_step()
                                
             logits = mlp(X=X,
-                         output_dim=OUTPUT_DIM,
+                         output_dim=output_dim,
                          is_training=is_training,
-                         fc_channel=[512, 512],
-                         reg_lambda=0.,
-                         dropout_keep_prob=1.)
+                         **param_dict['model_param'])
 
             Y_pred = slim.softmax(logits)
 
@@ -96,24 +103,55 @@ def main(_):
         # restoring from a checkpoint, saving to a checkpoint, and closing when done
         # or an error occurs.
         with tf.train.MonitoredTrainingSession(master=server.target,
-                                               is_chief=(
-                                                   FLAGS.task_index == 0),
+                                               is_chief=is_chief,
                                                checkpoint_dir="/tmp/train_logs",
                                                hooks=hooks) as mon_sess:
 
             # Get dataset handle
             train_handle = mon_sess.run(train_handle_tensor)
-            # valid_handle = mon_sess.run(valid_iterator.string_handle())
+            valid_handle = mon_sess.run(valid_handle_tensor)
 
+            acc_window = [0.]*TRAIN_ACC_WINDOW_SIZE
+            
+            batch_i = 0
             while not mon_sess.should_stop():
                 # Run a training step asynchronously.
                 # See `tf.train.SyncReplicasOptimizer` for additional details on how to
                 # perform *synchronous* training.
                 # mon_sess.run handles AbortedError in case of preempted PS.
-                train_accuracy, _ = mon_sess.run([accuracy, train_op],
-                                                 feed_dict={is_training: True,
-                                                            handle: train_handle, })
-                print("accuracy : {}".format(train_accuracy))
+                mon_sess.run(train_op,
+                             feed_dict={is_training: True,
+                                        handle: train_handle, })
+                if is_chief:
+                    train_accuracy = mon_sess.run(accuracy,
+                                                  feed_dict={is_training: False,
+                                                             handle: train_handle, })
+                    acc_window = acc_window[1:] + [train_accuracy]
+
+                    if batch_i % VERBOSE_INTERVAL == 0:
+                        recent_mean_train_accuracy = sum(acc_window)/len(acc_window)
+
+                        valid_i = 0 
+                        mon_sess.run(valid_iterator.initializer)
+                        valid_correct = 0 
+                        valid_num = 0
+
+                        while True:
+                            try:
+                                pred, batch_valid_correct = mon_sess.run([Y_pred, correct] 
+                                                                         feed_dict={is_training: False,
+                                                                                    handle: valid_handle, })
+                                valid_correct += batch_valid_correct
+                                valid_num += pred.shape[0]
+                                valid_i += 1
+                            except tf.errors.OutOfRangeError:
+                                break
+                        valid_accuracy = valid_correct/valid_num
+
+                        print("recent_mean_train_accuracy : {}".format(recent_mean_train_accuracy))
+                        print("valid_accuracy : {}".format(valid_accuracy)
+
+                batch_i += 1
 
 
 if __name__ == "__main__":
@@ -145,5 +183,12 @@ if __name__ == "__main__":
         default=0,
         help="Index of task within the job"
     )
+    parser.add_argument(
+        "--param_path",
+        type=str,
+        default="./param.json",
+        help="parameters of model"
+    )
+
     FLAGS, unparsed = parser.parse_known_args()
     tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
--- utils.py
+++ utils.py
@@ -1,3 +1,5 @@
+import json
+
 import tensorflow as tf
 
 
@@ -7,5 +9,10 @@ def calc_metric(Y, Y_pred, name=None):
             tf.argmax(Y_pred, 1), tf.argmax(Y, 1))
         accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float64))
     return accuracy, correct_prediction
+
+
+def load_json(path):
+    with open(path, "rt") as f:
+        return json.load(f)
 
 
