40e17235259f485dea596f15c9a029954d1af7cb
Sung-ju Kim <goddoe@goddoe2@gmail.com>
2018-02-28 17:34:10 +0900

    update script

--- run_wk_0.sh
+++ run_wk_0.sh
@@ -2,4 +2,5 @@
 python3 tf_distributed.py \
      --ps_hosts=localhost:2222 \
      --worker_hosts=localhost:2223 \
-     --job_name=worker --task_index=0
+     --job_name=worker --task_index=0 \
+     --param_path=./param.json
